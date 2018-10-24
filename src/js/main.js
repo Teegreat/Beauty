c0f6f199d1cdcc13907a967010ec68a92858284a
Teegreat <toyinatofarati64@gmail.com>
2018-10-24 09:52:53 +0100

    my first commit

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+// $(".carousel-inner").carousel_inner({
+//   interval: 2000
+// });
+
+// // $(document).ready(function () {
+// //   $(".navbar-nav li a").click(function(event) {
+// //     $(".navbar-collapse").collapse('hide');
+// //   });
+// // });
+
+// window.onload = carousel;
+
+// $('.carousel').carousel({
+//     pause: "false"
+// });
